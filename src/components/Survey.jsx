df844731621c3d76ab8dee443b5c63e47ca5e347
Shuga8 <lotocharles8@gmail.com>
2024-08-21 00:17:18 -0700

    persist

--- src/components/Survey.jsx
+++ src/components/Survey.jsx
@@ -11,6 +11,8 @@ const getQuestionId = (surveyList, activeIndex, qIndex) => {
 };
 
 const Survey = () => {
+  const [categoryLoading, setCategoryLoading] = useState(false);
+  const [questionsVisible, setQuestionsVisible] = useState(false);
   const [activeIndex, setActiveIndex] = useState(0);
   const [selectedOptions, setSelectedOptions] = useState({});
   const [errors, setErrors] = useState(false);
@@ -70,6 +72,7 @@ const Survey = () => {
   };
 
   const handleSaveAndContinue = () => {
+    setCategoryLoading(true);
     if (validateSurvey()) {
       const currentSurvey = surveyList[activeIndex];
       currentSurvey.questions.forEach((q, qIndex) => {
@@ -93,139 +96,172 @@ const Survey = () => {
         setSelectedOptions({});
         setErrors(false);
       } else {
-        console.log("The survey has finished.");
+        setTimeout(() => {
+          setQuestionsVisible(true);
+        }, 1600);
+        console.log(surveyList);
       }
     } else {
       console.error("Please answer all questions in the current category.");
     }
+
+    setTimeout(() => {
+      setCategoryLoading(false);
+    }, 1500);
   };
 
   const isLastSurvey = activeIndex === surveyList.length - 1;
 
   return (
-    <div>
-      <h1 className="text-3xl text-center py-4 text-cyan-600">ONT Survey</h1>
-      <div className="survey-widgets-container w-1/2 p-6 flex flex-row justify-center mx-auto overflow-x-scroll gap-1">
-        {surveyList.map((survey, index) => (
-          <div
-            key={index}
-            className={`survey-widget ${
-              activeIndex === index && !survey.isCompleted
-                ? "active-widget"
-                : ""
-            } ${survey.isCompleted ? "cursor-not-allowed" : "cursor-pointer"}`}
-            onClick={() => {
-              if (!survey.isCompleted) setActiveIndex(index);
-            }}
-          >
-            {index + 1}
-          </div>
-        ))}
-      </div>
-
-      <div className="surveys-container sm:px-1 py-9 md:px-6">
-        <div className="questions-container py-2">
-          <h2 className="text-center text-2xl text-cyan-600 mb-6 font-extrabold">
-            {surveyList[activeIndex].title}
-          </h2>
-          {surveyList[activeIndex].questions.map((q, qIndex) => (
-            <div key={qIndex} className="question mb-4">
-              <p className="mb-2">
-                <span className="text-xl font-medium text-gray-500">
-                  {getQuestionId(surveyList, activeIndex, qIndex)}
-                </span>
-                . {q.question}
-              </p>
-
-              <div className="input-group px-4 py-6">
-                {q.type === "open" && (
-                  <input
-                    type="text"
-                    className="w-full p-2 border"
-                    value={selectedOptions[qIndex] || ""}
-                    onChange={(e) => handleInputChange(qIndex, e.target.value)}
-                    placeholder="Your answer ..."
-                  />
-                )}
-                {q.type === "single_choice" && (
-                  <div>
-                    {q.options.map((option, oIndex) => (
-                      <div key={oIndex} className="flex items-center mb-2">
-                        <input
-                          type="radio"
-                          id={`q${qIndex}-o${oIndex}`}
-                          name={`q${qIndex}`}
-                          value={option}
-                          className="custom-radio mr-2"
-                          checked={selectedOptions[qIndex] === option}
-                          onChange={(e) =>
-                            handleSingleChoiceChange(qIndex, e.target.value)
-                          }
-                        />
-                        <label htmlFor={`q${qIndex}-o${oIndex}`}>
-                          {option}
-                        </label>
-                      </div>
-                    ))}
-                    {selectedOptions[qIndex] === "Others...please specify" && (
-                      <input
-                        type="text"
-                        className="w-full p-2 border"
-                        placeholder="Please specify"
-                        value={selectedOptions[`${qIndex}_other`] || ""}
-                        onChange={(e) =>
-                          handleOtherOptionChange(qIndex, e.target.value)
-                        }
-                      />
-                    )}
-                  </div>
-                )}
-                {q.type === "multiple_choice" && (
-                  <div>
-                    {q.options.map((option, oIndex) => (
-                      <div key={oIndex} className="flex items-center mb-2">
-                        <input
-                          type="checkbox"
-                          id={`q${qIndex}-o${oIndex}`}
-                          value={option}
-                          className="custom-checkbox mr-2"
-                          checked={
-                            selectedOptions[qIndex] &&
-                            selectedOptions[qIndex].includes(option)
-                          }
-                          onChange={() =>
-                            handleMultipleChoiceChange(qIndex, option)
-                          }
-                        />
-                        <label htmlFor={`q${qIndex}-o${oIndex}`}>
-                          {option}
-                        </label>
-                      </div>
-                    ))}
-                  </div>
-                )}
-              </div>
+    <>
+      <div hidden={questionsVisible}>
+        <h1 className="text-3xl text-center py-4 text-cyan-600">ONT Survey</h1>
+        <div className="survey-widgets-container w-1/2 p-6 flex flex-row justify-center mx-auto overflow-x-scroll gap-1">
+          {surveyList.map((survey, index) => (
+            <div
+              key={index}
+              className={`survey-widget ${
+                activeIndex === index && !survey.isCompleted
+                  ? "active-widget"
+                  : ""
+              } 
+            ${survey.isCompleted ? "cursor-not-allowed" : "cursor-pointer"}`}
+              onClick={() => {
+                if (!survey.isCompleted) setActiveIndex(index);
+              }}
+            >
+              {index + 1}
             </div>
           ))}
         </div>
 
-        {errors && (
-          <p className="text-red-500 text-center">
-            Please answer all questions in the current category.
-          </p>
-        )}
+        <div className="surveys-container sm:px-1 py-9 md:px-6">
+          <div className="questions-container py-2">
+            <h2 className="text-center text-2xl text-cyan-600 mb-6 font-extrabold">
+              {surveyList[activeIndex].title}
+            </h2>
 
-        <div className="button-list block float-right p-3">
-          <Button
-            variant="contained"
-            color="primary"
-            onClick={handleSaveAndContinue}
-          >
-            {isLastSurvey ? "Finish Survey" : "Save & Continue"}
-          </Button>
+            {}
+            {surveyList[activeIndex].questions.map((q, qIndex) => (
+              <div key={qIndex} className="question mb-4">
+                <p className="mb-2">
+                  <span className="text-xl font-medium text-gray-500">
+                    {getQuestionId(surveyList, activeIndex, qIndex)}
+                  </span>
+                  . {q.question}
+                </p>
+
+                <div className="input-group px-4 py-6">
+                  {q.type === "open" && (
+                    <input
+                      type={q.field}
+                      className="w-full p-2 border"
+                      value={selectedOptions[qIndex] || ""}
+                      onChange={(e) =>
+                        handleInputChange(qIndex, e.target.value)
+                      }
+                      placeholder="Your answer ..."
+                      maxLength={q.field == "number" ? "999999" : q.limit}
+                    />
+                  )}
+                  {q.type === "single_choice" && (
+                    <div>
+                      {q.options.map((option, oIndex) => (
+                        <div key={oIndex} className="flex items-center mb-2">
+                          <input
+                            type="radio"
+                            id={`q${qIndex}-o${oIndex}`}
+                            name={`q${qIndex}`}
+                            value={option}
+                            className="custom-radio mr-2"
+                            checked={selectedOptions[qIndex] === option}
+                            onChange={(e) =>
+                              handleSingleChoiceChange(qIndex, e.target.value)
+                            }
+                          />
+                          <label htmlFor={`q${qIndex}-o${oIndex}`}>
+                            {option}
+                          </label>
+                        </div>
+                      ))}
+                      {selectedOptions[qIndex] ===
+                        "Others...please specify" && (
+                        <input
+                          type="text"
+                          className="w-full p-2 border"
+                          placeholder="Please specify"
+                          value={selectedOptions[`${qIndex}_other`] || ""}
+                          onChange={(e) =>
+                            handleOtherOptionChange(qIndex, e.target.value)
+                          }
+                        />
+                      )}
+                    </div>
+                  )}
+                  {q.type === "multiple_choice" && (
+                    <div>
+                      {q.options.map((option, oIndex) => (
+                        <div key={oIndex} className="flex items-center mb-2">
+                          <input
+                            type="checkbox"
+                            id={`q${qIndex}-o${oIndex}`}
+                            value={option}
+                            className="custom-checkbox mr-2"
+                            checked={
+                              selectedOptions[qIndex] &&
+                              selectedOptions[qIndex].includes(option)
+                            }
+                            onChange={() =>
+                              handleMultipleChoiceChange(qIndex, option)
+                            }
+                          />
+                          <label htmlFor={`q${qIndex}-o${oIndex}`}>
+                            {option}
+                          </label>
+                        </div>
+                      ))}
+                    </div>
+                  )}
+                </div>
+              </div>
+            ))}
+          </div>
+
+          {errors && (
+            <p className="text-red-500 text-center">
+              Please answer all questions in the current category.
+            </p>
+          )}
+
+          <div className="button-list block float-right p-3">
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={handleSaveAndContinue}
+              disabled={categoryLoading}
+            >
+              {!categoryLoading ? (
+                <span>
+                  {isLastSurvey ? "Finish Survey" : "Save & Continue"}
+                </span>
+              ) : (
+                <span className="material-symbols-outlined spinner">
+                  progress_activity
+                </span>
+              )}
+            </Button>
+          </div>
         </div>
       </div>
-    </div>
+
+      <div
+        className={`p-4 text-xl w-full h-screen place-items-center justify-center items-center text-green-600 text-center ${
+          questionsVisible ? "flex" : "hidden"
+        }`}
+      >
+        ONT Survey Has Been Completed Successfully!
+      </div>
+    </>
   );
 };
 
