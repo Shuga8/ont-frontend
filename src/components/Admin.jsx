61392441d5a2e5f40bcb5682b99e4d25e293cb82
Shuga8 <lotocharles8@gmail.com>
2024-08-22 20:12:22 -0700

    completed Survey

--- src/components/Admin.jsx
+++ src/components/Admin.jsx
@@ -11,11 +11,14 @@ const Admin = () => {
       <SideBar />
       <div className="elements-container mt-14">
         <Loader />
-        <div className="w-full h-10 px-4 py-8 text-gray-700   border-b-2 border-gray-300 flex flex-row items-center place-items-center ">
-          <span>ONT&nbsp;/&nbsp;</span>
-          <Link to={"/admin/"} className="underline text-blue-500">
-            Dashboard
-          </Link>
+        <div className="w-full h-10 px-4 py-8 text-gray-700   border-b-2 border-gray-300 flex flex-row items-center place-items-center justify-between ">
+          <span className="font-bold text-lg text-gray-900">Dasbhoard</span>
+          <span>
+            ONT&nbsp;/&nbsp;
+            <Link to={"/admin/"} className="underline text-blue-500">
+              Dashboard
+            </Link>
+          </span>
         </div>
 
         <div className="container-content  px-1 py-2 md:px-6 md:py-5">
