afa680891b7d21e31cc0371d6419d869b5cc88d1
Shuga8 <lotocharles8@gmail.com>
2024-08-26 17:31:21 +0100

    login hook template

--- src/hooks/useLogin.jsx
+++ src/hooks/useLogin.jsx
@@ -1,19 +1,57 @@
+import { useState } from "react";
+import { useAuthContext } from "./useAuthContext";
+
 export const useLogin = () => {
+  const [error, setError] = useState(null);
+  const [success, setSuccess] = useState(null);
+  const [isLoading, setIsloading] = useState(false);
+
+  const { dispatch } = useAuthContext();
+
   const login = async (email, password) => {
-    const myHeaders = new Headers();
-    myHeaders.append("Content-Type", "application/json");
+    setError(null);
+    setSuccess(null);
+    setIsloading(true);
 
-    const data = JSON.stringify({ email, password });
+    if ((email == "") | (email == null)) {
+      setError("please enter your email address");
+      return;
+    } else if (pass == "" || pass == null) {
+      setError("please enter your password");
+    } else {
+      try {
+        const myHeaders = new Headers();
+        myHeaders.append("Content-Type", "application/json");
 
-    const loginUser = await fetch("/v1/auth/login", {
-      method: "POST",
-      headers: myHeaders,
-      body: data,
-    });
-    const response = await loginUser.json();
+        const data = JSON.stringify({ email, password });
 
-    console.log(response);
+        const loginUser = await fetch(
+          "https://ont-survey-tracker-development.up.railway.app/v1/auth/login",
+          {
+            method: "POST",
+            headers: myHeaders,
+            body: data,
+          }
+        );
+
+        const response = await loginUser.json();
+
+        if (!loginUser.ok) {
+          throw new Error(response.message);
+        } else {
+          localStorage.setItem("user", JSON.stringify(response.data));
+          dispatch({ type: "LOGIN", payload: response.data });
+          setSuccess("login successfull");
+        }
+
+        console.log(response);
+      } catch (err) {
+        setError(err);
+      } finally {
+        setIsloading(false);
+      }
+    }
   };
 
-  return { login };
+  return { login, isLoading, error };
 };
