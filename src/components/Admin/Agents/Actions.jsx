7b05b48f577d62c2dc834ccac1f15472693ffe67
Shuga8 <lotocharles8@gmail.com>
2024-08-26 13:44:32 +0100

    new agent form

---
+++ src/components/Admin/Agents/Actions.jsx
@@ -0,0 +1,171 @@
+import React, { useState } from "react";
+import { FiSearch } from "react-icons/fi";
+import { Button } from "@mui/material";
+import { MdFormatListBulletedAdd } from "react-icons/md";
+import { PiUserCirclePlusDuotone } from "react-icons/pi";
+
+const Actions = () => {
+  const showNewSurveyForm = (e) => {
+    document.querySelector(".new-survey-form").classList.remove("hidden");
+  };
+  window.addEventListener("click", function (e) {
+    if (e.target == document.querySelector(".new-survey-form")) {
+      document.querySelector(".new-survey-form").classList.add("hidden");
+    }
+  });
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <>
+      <div className="flex justify-end p-3 flex-row gap-x-2">
+        <Button
+          variant="outlined"
+          color="primary"
+          className="flex flex-row gap-x-2"
+          onClick={showNewSurveyForm}
+        >
+          <span className="hidden md:block text-base">New Agent</span>
+          <PiUserCirclePlusDuotone className="text-blue-700 text-2xl" />
+        </Button>
+        <div className="search-form">
+          <form className="max-w-md min-w-72 md:min-w-80 mx-auto md:mx-0">
+            <label
+              htmlFor="default-search"
+              className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
+            >
+              Search
+            </label>
+            <div className="relative">
+              <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+                <FiSearch className="w-4 h-4 text-gray-500" />
+              </div>
+              <input
+                type="search"
+                id="default-search"
+                className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 "
+                placeholder="Search by email"
+                required
+              />
+              <button
+                type="submit"
+                className="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              >
+                Search
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+      <div className="new-survey-form  hidden">
+        <div class="flex items-center justify-center px-6 py-8 mx-auto md:h-screen min-w-96 lg:py-0">
+          <div class="w-full  rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 bg-gray-800 border-gray-700 min-w-96">
+            <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
+              <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                Create an account for agent
+              </h1>
+              <form
+                class="space-y-4 md:space-y-6"
+                action="#"
+                method="POST"
+                onSubmit={handleSubmit}
+              >
+                <div>
+                  <label
+                    htmlFor="email"
+                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  >
+                    Email
+                  </label>
+                  <input
+                    type="email"
+                    name="email"
+                    id="email"
+                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    placeholder="name@company.com"
+                    required=""
+                  />
+                </div>
+                <div>
+                  <label
+                    htmlFor="firstname"
+                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  ></label>
+                  <input
+                    type="text"
+                    name="firstname"
+                    id="firstname"
+                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    placeholder="firstname"
+                    required=""
+                  />
+                </div>
+                <div>
+                  <label
+                    htmlFor="lastname"
+                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  ></label>
+                  <input
+                    type="text"
+                    name="lastname"
+                    id="lastname"
+                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    placeholder="lastname"
+                    required=""
+                  />
+                </div>
+                <div>
+                  <label
+                    htmlFor="password"
+                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  >
+                    Password
+                  </label>
+                  <input
+                    type="password"
+                    name="password"
+                    id="password"
+                    placeholder="••••••••"
+                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    required=""
+                  />
+                </div>
+                <div>
+                  <label
+                    htmlFor="confirm-password"
+                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  >
+                    Confirm password
+                  </label>
+                  <input
+                    type="confirm-password"
+                    name="confirm-password"
+                    id="confirm-password"
+                    placeholder="••••••••"
+                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    required=""
+                  />
+                </div>
+
+                <Button
+                  variant="contained"
+                  color="primary"
+                  type="submit"
+                  class="w-full text-white bg-primary-600 hover:bg-primary-700 0 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+                >
+                  Create an account
+                </Button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Actions;
--- src/components/Admin/Agents/Agents.jsx
+++ src/components/Admin/Agents/Agents.jsx
@@ -3,8 +3,8 @@ import { Loader, SideBar } from "../index";
 import { Link } from "react-router-dom";
 import { GrFormNext, GrFormPrevious } from "react-icons/gr";
 import { IoTrashOutline } from "react-icons/io5";
-import { TbWorldSearch } from "react-icons/tb";
 import { TbPencilCog } from "react-icons/tb";
+import Actions from "./Actions";
 
 const Agents = () => {
   return (
@@ -23,6 +23,7 @@ const Agents = () => {
         </div>
 
         <div className="container-content  px-1 py-2 md:px-6 md:py-5">
+          <Actions />
           <div className="rounded-sm border border-stroke bg-white px-5 pb-6 pt-6 shadow-default dark:border-strokedark dark:bg-boxdark sm:px-7 xl:pb-6">
             <div className="flex flex-col">
               <div className="grid grid-cols-3 rounded-sm bg-gray-2 dark:bg-meta-4 sm:grid-cols-5 bg-slate-200">
