c931f387dc5ba43ff13297ea0b70ff449aae53cd
Shuga8 <lotocharles8@gmail.com>
2024-08-26 14:00:43 +0100

    another sudo login

--- src/components/Admin/SideBar.jsx
+++ src/components/Admin/SideBar.jsx
@@ -118,23 +118,24 @@ const SideBar = () => {
                   </div>
                   <ul className="py-1" role="none">
                     <li>
-                      <a
-                        href="#"
+                      <Link
+                        to={"/admin/settings"}
                         className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white"
                         role="menuitem"
                       >
                         Settings
-                      </a>
+                      </Link>
                     </li>
 
                     <li>
-                      <a
+                      <Link
                         href="#"
                         className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white"
                         role="menuitem"
+                        onClick={logoutAction}
                       >
                         Sign out
-                      </a>
+                      </Link>
                     </li>
                   </ul>
                 </div>
