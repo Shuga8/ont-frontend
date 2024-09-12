2e7363d1058af8c510416ab734cbde9d41aadc19
Shuga8 <lotocharles8@gmail.com>
2024-09-12 16:38:12 +0100

    pending page pagination

--- src/components/Admin/Survey/Pending.jsx
+++ src/components/Admin/Survey/Pending.jsx
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { ErrorToast, Loader, SideBar } from "../index";
+import { GrFormNext, GrFormPrevious } from "react-icons/gr";
 import { GoTasklist } from "react-icons/go";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import Search from "./Search";
 import useGetRespondents from "../Api/Respondents";
 import TableSkeleton from "../../Skeleton/TableSkeleton";
@@ -12,6 +13,11 @@ const getSearchValue = () => {
   return params.get("search") || null;
 };
 
+const getPageValue = () => {
+  const params = new URLSearchParams(window.location.search);
+  return params.get("page") || null;
+};
+
 const Pending = () => {
   const [respondents, setRespondents] = useState(null);
   const [isErrorActive, setErrorActive] = useState(false);
@@ -20,23 +26,29 @@ const Pending = () => {
   const { loadingGetRespondents, getRespondents } = useGetRespondents();
   const { loadingPhoneRespondents, errorPhone, getPhoneRespondent } =
     useGetRespondentByPhone(getSearchValue(), "pending");
+  const [pagination, setPagination] = useState(null);
+  const [page, setPage] = useState(getPageValue());
+  const navigate = useNavigate();
 
   useEffect(() => {
     const fetchRespondents = async () => {
       if (getSearchValue() == null) {
         setRespondents(null);
         const { pagination, filteredRespondents } = await getRespondents(
-          "pending"
+          "pending",
+          page
         );
         setRespondents(filteredRespondents);
+        setPagination(pagination);
       } else {
         setRespondents(null);
         const { pagination, filteredRespondents } = await getPhoneRespondent();
         setRespondentByPhone(filteredRespondents);
+        setPagination(null);
       }
     };
     fetchRespondents();
-  }, []);
+  }, [page]);
 
   useEffect(() => {
     if (error) {
@@ -49,12 +61,79 @@ const Pending = () => {
     }
   }, [error]);
 
+  const handlePageChange = (page) => {
+    const currentParams = new URLSearchParams(window.location.search);
+    currentParams.set("page", page);
+    navigate(`?${currentParams.toString()}`);
+  };
+
+  if (respondents == null && respondentByPhone == null) {
+    return (
+      <>
+        <SideBar />
+        <div className="elements-container mt-14">
+          <ErrorToast isActive={isErrorActive} message={error} />
+          <div className="w-full h-10 px-4 py-8 text-gray-700   border-b-2 border-gray-300 flex flex-row items-center place-items-center justify-between">
+            <span className="font-bold text-lg text-gray-900">
+              Pending Surveys
+            </span>
+            <span>
+              ONT&nbsp;/&nbsp;Survey&nbsp;/
+              <Link to={"/admin/survey/pending"} className=" text-yellow-600">
+                &nbsp;Pending
+              </Link>
+            </span>
+          </div>
+          <div className="container-content  px-1 py-2 md:px-6 md:py-5">
+            <Search />
+            <div className="rounded-sm border border-stroke bg-white px-5 pb-6 pt-6 shadow-default dark:border-strokedark dark:bg-boxdark sm:px-7 xl:pb-6">
+              <div className="flex flex-col">
+                <div className="grid grid-cols-3 rounded-sm bg-gray-2 dark:bg-meta-4 sm:grid-cols-5 bg-slate-200">
+                  <div className="p-2 xl:p-5">
+                    <h5 className="text-sm font-medium uppercase sm:text-base text-stone-900">
+                      ID
+                    </h5>
+                  </div>
+
+                  <div className="p-2 xl:p-5 hidden md:block">
+                    <h5 className="text-sm font-medium uppercase sm:text-base text-stone-900">
+                      Respondent
+                    </h5>
+                  </div>
+
+                  <div className="p-2 xl:p-5">
+                    <h5 className="text-sm font-medium uppercase sm:text-base text-stone-900">
+                      Phone
+                    </h5>
+                  </div>
+
+                  <div className="p-2 xl:p-5 text-center hidden md:block">
+                    <h5 className="text-sm font-medium uppercase sm:text-base text-stone-900">
+                      Gender
+                    </h5>
+                  </div>
+
+                  <div className="p-2 xl:p-5 text-center">
+                    <h5 className="text-sm text-center md:text-left  font-medium uppercase sm:text-base text-stone-900">
+                      Actions
+                    </h5>
+                  </div>
+                </div>
+
+                <TableSkeleton count={6} />
+              </div>
+            </div>
+          </div>
+        </div>
+      </>
+    );
+  }
+
   return (
     <>
       <SideBar />
       <div className="elements-container mt-14">
         <ErrorToast isActive={isErrorActive} message={error} />
-        <Loader />
         <div className="w-full h-10 px-4 py-8 text-gray-700   border-b-2 border-gray-300 flex flex-row items-center place-items-center justify-between">
           <span className="font-bold text-lg text-gray-900">
             Pending Surveys
@@ -239,33 +318,57 @@ const Pending = () => {
               )}
             </div>
 
-            {/* <div className="table-pagination pb-4 pt-6 flex flex-row justify-between gap-x-2 place-items-center">
-              <p className="text-gray-700">Showing 1-5 of 25</p>
-              <nav>
-                <ul className="flex items-center gap-x-2 h-8 text-sm">
-                  <li>
-                    <a
-                      href="#"
-                      className="flex items-center justify-center px-3 h-8 ms-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-full hover:bg-gray-100 hover:text-gray-700"
-                    >
-                      <span className="sr-only">Previous</span>
-                      <GrFormPrevious />
-                    </a>
-                  </li>
+            {pagination && (
+              <div className="table-pagination pb-4 pt-6 flex flex-row justify-between gap-x-2 place-items-center">
+                <p className="text-gray-700">
+                  Page <b>{pagination.page}</b> of{" "}
+                  <b>{pagination.totalPages} </b> in {" ("}
+                  {pagination.page != pagination.totalPages
+                    ? respondents.length * pagination.page
+                    : pagination.totalResults}{" "}
+                  out of {pagination.totalResults}
+                  {" )"} results
+                </p>
+                <nav>
+                  <ul className="flex items-center gap-x-2 h-8 text-sm">
+                    <li>
+                      <Link
+                        to={pagination.prevPage ?? "#"}
+                        className="flex items-center justify-center px-3 h-8 ms-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-full hover:bg-gray-100 hover:text-gray-700"
+                        onClick={(e) => {
+                          e.preventDefault();
+                          if (pagination.prevPage) {
+                            setPage(pagination.prev);
+                            handlePageChange(pagination.prev);
+                          }
+                        }}
+                      >
+                        <span className="sr-only">Previous</span>
+                        <GrFormPrevious />
+                      </Link>
+                    </li>
 
-                  <li>
-                    <a
-                      href="#"
-                      className="flex items-center justify-center px-3 h-8 ms-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-full hover:bg-gray-100 hover:text-gray-700"
-                    >
-                      <span className="sr-only">Next</span>
+                    <li>
+                      <Link
+                        to={pagination.nextPage ?? "#"}
+                        className="flex items-center justify-center px-3 h-8 ms-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-full hover:bg-gray-100 hover:text-gray-700"
+                        onClick={(e) => {
+                          e.preventDefault();
+                          if (pagination.nextPage) {
+                            setPage(pagination.next);
+                            handlePageChange(pagination.next);
+                          }
+                        }}
+                      >
+                        <span className="sr-only">Next</span>
 
-                      <GrFormNext />
-                    </a>
-                  </li>
-                </ul>
-              </nav>
-            </div> */}
+                        <GrFormNext />
+                      </Link>
+                    </li>
+                  </ul>
+                </nav>
+              </div>
+            )}
           </div>
         </div>
       </div>
