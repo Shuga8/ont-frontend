309f28b57b7e5db717687b24c4b1c61f19523fc4
Shuga8 <lotocharles8@gmail.com>
2024-09-21 21:24:21 +0100

    info redesign

--- src/components/Admin/Widgets/Info.jsx
+++ src/components/Admin/Widgets/Info.jsx
@@ -38,26 +38,41 @@ const Info = () => {
           <img src={logo} className="w-10 h-10 mx-auto my-4" alt="Unicef" />
           <h3 className="text-sm md:text-xl font-extrabold text-center">
             Welcome to the{" "}
-            <span className="italic font-bold text-red-500">
-              UNICEF SURVEY{" "}
-            </span>
+            <span className="italic font-bold text-red-500">ONT SURVEY </span>
             administrator app.
           </h3>
           <p className="text-green-600 text-center my-4 text-sm">
-            In patnership with GLO and Oxford, we aim to make the world a better
-            place for future generations...
+            In patnership with UNICEF, GLO and Oxford, we aim to make the world
+            a better place for future generations...
           </p>
           <p className="text-green-600 text-center my-4 text-sm">
-            With the software, we keep track of the voices of individuals, who
-            are caregivers to children , to get a common ground of situations,
-            resulting in how to help, and building future prospects.
+            With this application software, we keep track of the voices of
+            individuals, who are caregivers to children , to get a common ground
+            of situations, resulting in how to help, and building future
+            prospects.
           </p>
           <p className="text-green-600 text-center my-4 text-sm">
-            Whether as an admin or agent, we appriciate you for being in this
+            Whether as an admin or agent, we appreciate you for being in this
             jorney with us.
           </p>
 
-          <div className="mt-10 mb-2">
+          <div className="developer ">
+            <p className="text-red-600 text-base font-extrabold text-center mt-10 uppercase">
+              Powered By
+            </p>
+
+            <img
+              src={one_thought}
+              alt=""
+              className="w-10 h-10 rounded-full mx-auto"
+            />
+
+            <p className="text-teal-600 text-lg italic my-2 font-bold text-center">
+              ONE THOUGHT TECHNOLOGIES
+            </p>
+          </div>
+
+          <div className="mt-10 mb-2 flex flex-row justify-center w-full">
             <Button
               variant="contained"
               color="primary"
@@ -67,17 +82,6 @@ const Info = () => {
               Continue <BiSolidSkipNextCircle />
             </Button>
           </div>
-        </div>
-        <div className="developer float-end">
-          <p className="text-red-600 text-xs font-bold text-center mt-10">
-            Powered By
-          </p>
-
-          <img
-            src={one_thought}
-            alt=""
-            className="w-10 h-10 rounded-full mx-auto"
-          />
         </div>
       </div>
     </div>
